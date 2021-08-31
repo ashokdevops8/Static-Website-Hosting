851ee6353655def0a917cef35dec5191dd38e143
ashokdevops8 <89518458+ashokdevops8@users.noreply.github.com>
2021-08-31 12:52:08 +0530

    Delete ecs-terraform directory

--- ecs-terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "key_name" {
-  type        = string
-  description = "The name for ssh key, used for aws_launch_configuration"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "The name of AWS ECS cluster"
-}
